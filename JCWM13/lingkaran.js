c526f5184c049526d61e448e368abea1bfe2ea39
Abdi-01 <abdialghi@gmail.com>
2020-04-28 15:49:50 +0700

    Day02Mentoring

---
+++ JCWM13/lingkaran.js
@@ -0,0 +1,6 @@
+const pi = 22 / 7
+var r = 10
+let luas_lingkaran = pi * Math.pow(r, 2)
+let keliling_lingkaran = pi * r * 2
+console.log("Luas Lingkaran =", luas_lingkaran)
+console.log("Keliling Lingkaran =", keliling_lingkaran)
