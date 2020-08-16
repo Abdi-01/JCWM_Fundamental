27b5c26ff125673f512fcd8fe9f9bc4075939283
Abdi-01 <abdialghi@gmail.com>
2020-08-16 11:39:59 +0700

    day10_miniGame

---
+++ JCAH-0102/TrainingDay/day10_mentoring.js
@@ -0,0 +1,82 @@
+//Review function
+
+//Cara 1 : keyword function
+// function penjumlahan(param1, param2) {
+//     let hasil = param1 + param2
+//     return hasil
+// }
+function penjumlahan(param1, param2) {
+    let hasil = param1 + param2
+    // console.log(hasil)
+}
+
+// console.log(penjumlahan(2, 1))
+penjumlahan(3, 2)
+
+//Cara 2 : function expression
+var pengurangan = function (param1, param2) {
+    console.log(param1 - param2)
+}
+
+pengurangan(5, 2)
+
+//Cara 3 : arrow function
+jumlah = (param1, param2) => param1 + param2
+let kurang = (param1, param2) => { return param1 - param2 }
+console.log(jumlah(5, 2))
+
+//Penempatan variable
+// let global //global variable : bisa diakses dimana saja selama dalam satu file program
+
+// function coba(params) {
+//     let scope //scope variable : hanya bisa diakses didalam scope function
+//     global //memanggil variable global
+
+//     for (let index = 0; index < array.length; index++) {
+//         let element = array[index]; // variable yg hanya bisa dibaca didalam scope for looping
+//         for (let index = 0; index < array.length; index++) {
+//             scope
+//             let a
+//             element
+//         }
+//     }
+//     element//tidak bisa diakses karena diluar scope for loop
+// }
+
+//Ternary operator
+// 2 > 0 ? console.log("2 lebih dari 0") : console.log("2 tidak lebih dari 0")
+// let hasil =
+//     2 > 0 ?
+//         2 + 1 == 3 ?
+//             "hasil 3"
+//             : "tidak 3"
+//         : 0 == 0 ? "Ini 0" : null
+
+// if (condition) {
+//     if (condition) {
+
+//     } else {
+
+//     }
+// } else {
+//     if (condition) {
+
+//     } else {
+
+//     }
+// }
+// console.log(hasil)
+
+//Map dan Filter
+
+let angka = [2, 3, 4, 5, 6]
+
+let angkaNew = angka.map((item) => {
+    if (item % 2 === 0) {
+        return item
+    }
+})
+let angkaCari = angka.filter((item) => item == 2)
+
+console.log(angkaNew)
+console.log(angkaCari)
