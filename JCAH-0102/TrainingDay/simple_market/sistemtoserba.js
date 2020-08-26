cf9d2c3aa6a8fb94d7e66363fe81c60e27bda2c3
Abdi-01 <abdialghi@gmail.com>
2020-08-26 21:46:52 +0700

    simple_market_req02

--- JCAH-0102/TrainingDay/simple_market/sistemtoserba.js
+++ JCAH-0102/TrainingDay/simple_market/sistemtoserba.js
@@ -14,6 +14,24 @@ let dbUser = [
     }
 ]
 
+//Menyimpan data produk
+let dbProduk = [
+    {
+        namaProduk: "Good Time",
+        foto: "http://arnotts.co.id/images/image-goodtime/good_time_1550214863.png",
+        deskripsi: "Biskuit nikmat diwaktu yang tepat",
+        stock: 20,
+        harga: 6500
+    },
+    {
+        namaProduk: "Oreo",
+        foto: "https://static.republika.co.id/uploads/images/inpicture_slide/oreo-supreme-tidak-dijual-secara-resmi-di_200514190509-631.png",
+        deskripsi: "Oreo bikin jadi SULTAN",
+        stock: 15,
+        harga: 30000
+    }
+]
+
 //Menyembunyikan tampilan menu
 document.getElementById("regis-page").style.display = "none"
 document.getElementById("login-page").style.display = "none"
@@ -31,6 +49,17 @@ class DB_User {
     }
 }
 
+class DB_Produk {
+    constructor(_namaProduk, _foto, _deskripsi, _stock, _harga) {
+        this.namaProduk = _namaProduk;
+        this.foto = _foto;
+        this.deskripsi = _deskripsi;
+        this.stock = _stock;
+        this.harga = _harga;
+    }
+}
+
+////////////////////////////REQUIREMENT USER//////////////////////////////
 //Fungsi menu utama
 btMenu = (menu) => {
     if (menu == "regis") {
@@ -52,7 +81,7 @@ btRegis = () => {
     if (form.elements[0].value == '' || form.elements[1].value == '' || form.elements[2].value == '') {
         alert("Tolong isi semua form yang ada")
     } else {
-        if(cek.length == 0){
+        if (cek.length == 0) {
             if (form.elements[1].value.includes('@')) {
                 if (form.elements[2].value.length >= 4) {
                     dbUser.push(new DB_User(form.elements[0].value, form.elements[1].value, form.elements[2].value))
@@ -63,7 +92,7 @@ btRegis = () => {
             } else {
                 alert("Email Salah")
             }
-        }else{
+        } else {
             alert("Username Sudah Digunakan")
         }
     }
@@ -95,4 +124,76 @@ btLogin = () => {
     } else {
         alert("Tolong isi semua form yang ada")
     }
+}
+
+//////////////////////////REQUIREMENT PRODUK////////////////////////////////
+btTambahProduk = () => {
+    let formProduk = document.getElementById("formProduk")
+    if (formProduk.elements[0].value == '' || formProduk.elements[1].value == '' || formProduk.elements[2].value == '' || formProduk.elements[3].value == '' || formProduk.elements[4].value == '') {
+        alert("Isi semua form")
+    } else {
+        dbProduk.push(new DB_Produk(formProduk[0].value, formProduk[1].value, formProduk[2].value, parseInt(formProduk[3].value), parseInt(formProduk[4].value)))
+        printProduk()
+        console.table(dbProduk)
+    }
+}
+
+printProduk = (idx, dataProduk = dbProduk) => {
+    let tableElement = ''
+    dataProduk.forEach((item, index) => {
+        if (idx == index) {
+            tableElement += `
+            <tr>
+                <td>${index + 1}</td>
+                <td><input type="text" id="fotoBaru" placeholder="Foto Produk Baru" value="${item.foto}"/></td>
+                <td><input type="text" id="namaBaru" placeholder="Nama Produk Baru" value="${item.namaProduk}"/></td>
+                <td><input type="text" id="deskripsiBaru" placeholder="Deskripsi Baru" value="${item.deskripsi}"/></td>
+                <td><input type="number" id="stockBaru" placeholder="Stock Baru" value="${item.stock}"/></td>
+                <td><input type="number" id="hargaBaru" placeholder="Nama Produk Baru" value="${item.harga}"/></td>
+                <td><button type="button" onclick="btSimpan(${index})">Simpan</button><button type="button" onclick="printProduk()">Batal</button></td>
+            </tr>    `
+        } else {
+            tableElement += `
+            <tr>
+                <td>${index + 1}</td>
+                <td><img src="${item.foto}" width="120px" alt="foto_produk"/></td>
+                <td>${item.namaProduk}</td>
+                <td>${item.deskripsi}</td>
+                <td>${item.stock}</td>
+                <td>Rp. ${item.harga.toLocaleString()}</td>
+                <td><button type="button" onclick="btEditProduk(${index})">Edit 🔧</button><button type="button" >Hapus 🗑</button></td>
+            </tr>`
+        }
+    })
+
+    document.getElementById("listProduk").innerHTML = tableElement
+}
+
+printProduk()
+
+btDeleteProduk = idx => {
+    dbProduk.splice(idx, 1)
+    printProduk()
+}
+
+btEditProduk = (idx) => {
+    console.log("Index data : ", idx)
+    printProduk(idx)
+}
+
+btSimpan = (idx) => {
+    dbProduk[idx].foto = document.getElementById("fotoBaru").value
+    dbProduk[idx].namaProduk = document.getElementById("namaBaru").value
+    dbProduk[idx].deskripsi = document.getElementById("deskripsiBaru").value
+    dbProduk[idx].stock = parseInt(document.getElementById("stockBaru").value)
+    dbProduk[idx].harga = parseInt(document.getElementById("hargaBaru").value)
+    printProduk()
+}
+
+btCariProduk = () => {
+    let pencarian = dbProduk.filter((item, index) => {
+        return item.namaProduk.toLowerCase().includes(document.getElementById("cariProduk").value.toLowerCase())
+    })
+    // console.log(pencarian)
+    printProduk(null, pencarian)
 }
